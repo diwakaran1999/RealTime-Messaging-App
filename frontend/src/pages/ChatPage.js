fa83a4ef25af1e6a65fe5f4c6a5e7b90e818e2fa
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2023-09-19 00:46:47 +0530

    update

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,14 +1,24 @@
+import { Box } from "@chakra-ui/react";
 import { ChatState } from "../Context/ChatProvider";
+import MyChats from "../components/MyChats";
+import ChatBox from "../components/ChatBox";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
 
 const ChatPage = () => {
     const { user } = ChatState();
 
     return <div style={{width: "100%"}}>
-        {/* {user && <SideDrawer/>} */}
+        {user && <SideDrawer/>}
 
-        <Box>
-            {/* {user && <MyChats/>} */}
-            {/* {user && <ChatBox/>} */}
+        <Box
+            d="flex"
+            justifyContent='space-between'
+            w='100%'
+            h='90vh'
+            p='10px'
+        >
+            {user && <MyChats/>}
+            {user && <ChatBox/>}
         </Box>
     </div>;
     
