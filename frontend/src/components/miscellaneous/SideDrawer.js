2d2e2ca5478ba606093a58da6f81cda25b67b9dc
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2023-10-04 23:26:45 +0530

    update

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,6 +1,7 @@
 import { Menu, MenuButton } from '@chakra-ui/menu';
 import { MenuItem, MenuList, MenuDivider } from '@chakra-ui/menu';
 import { Tooltip } from '@chakra-ui/tooltip';
+import axios from "axios";
 import { Box, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
@@ -9,6 +10,9 @@ import { Avatar } from "@chakra-ui/avatar";
 import { ChatState } from '../../Context/ChatProvider';
 import ProfileModal from './ProfileModal';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Input, useDisclosure, useToast} from '@chakra-ui/react';
+import ChatLoading from '../ChatLoading';
+import UserListItem from '../UserAvatar/UserListItem';
 
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
@@ -18,10 +22,53 @@ const SideDrawer = () => {
 
   const { user } = ChatState();
   const history = useHistory();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const logoutHandler = () => {
     localStorage.removeItem("userInfo");
     history.push("/");
+  };
+
+  const toast = useToast();
+
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please Enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
+
+    try {
+      setLoading(true)
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+
+  };
+
+  const accessChat = (userId) => {
+    
   };
 
   return (
@@ -39,7 +86,7 @@ const SideDrawer = () => {
           label="Search Users to chat"
           hasArrow
           placement='bottom-end'>
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={onOpen}>
           <i class="fas fa-search"></i>
           <Text display={{base:"none",md:"flex"}} px='4'>Search User</Text>
         </Button>
@@ -69,9 +116,37 @@ const SideDrawer = () => {
               <MenuItem onClick={logoutHandler}>Logout</MenuItem>
             </MenuList>
           </Menu>
-
         </div>
       </Box>
+
+      <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+        <DrawerBody>
+          <Box display='flex' pb={2}>
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e)=>setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+          </Box>
+            {loading ? <ChatLoading /> : 
+              (
+                searchResult?.map((user) => (
+                  <UserListItem
+                    key={user._id}
+                    user={user}
+                    handleFunction={() => accessChat(user._id)}
+                  />
+                ))
+              )
+            }
+        </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
