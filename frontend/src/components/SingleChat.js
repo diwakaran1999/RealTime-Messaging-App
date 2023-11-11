118fc5be1c2b593e995f0464b57926bb4608b3e6
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2023-11-11 12:04:12 +0530

    updateGroupChat

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -4,6 +4,7 @@ import { Box, IconButton, Text } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { getSender, getSenderFull } from '../config/ChatLogics';
 import ProfileModal from './miscellaneous/ProfileModal';
+import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal';
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     const { user, selectedChat, setSelectedChat } = ChatState();
@@ -35,13 +36,26 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                         ) : (
                                 <>
                                     {selectedChat.chatName.toUpperCase()}
-                                    {/* {<UpdateGroupChatModal
+                                    {<UpdateGroupChatModal
                                         fetchAgain={fetchAgain}
                                         setFetchAgain={setFetchAgain}
-                                    />} */}
+                                    />}
                                 </>
                         )}
                     </Text>
+                    <Box
+                        display="flex"
+                        flexDir="column"
+                        justifyContent="flex-end"
+                        p={3}
+                        bg="#E8E8E8"
+                        w="100%"
+                        h="100%"
+                        borderRadius="lg"
+                        overflow="hidden"
+                    >
+                        {/* {message} */}
+                    </Box>
                 </>
             ) : (
                 <Box display="flex" alignItems="center" justitfyContent="center" h="100%">
